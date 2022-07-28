3af4fbdb05360bfa257487d9551c46cca248cc43
Иван Зорин <local.zet@yandex.ru>
2022-07-28 15:09:36 +0500

    Initial commit

---
+++ src/Response.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package     localzet HTTP Client
+ * @link        https://localzet.gitbook.io
+ * 
+ * @author      localzet <creator@localzet.ru>
+ * 
+ * @copyright   Copyright (c) 2018-2020 Zorin Projects 
+ * @copyright   Copyright (c) 2020-2022 NONA Team
+ * 
+ * @license     https://www.localzet.ru/license GNU GPLv3 License
+ */
+
+namespace localzet\HTTP;
+
+use localzet\PSR\PSR7\Response as PSR_Response;
+
+/**
+ * Class HTTP\Response
+ * @package localzet\HTTP
+ */
+class Response extends PSR_Response
+{
+}
